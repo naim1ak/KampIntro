5d26b7b1a5479816feb58ac5cd3d2a99cd3feda0
PRO\naim.ak <naim.ak@bdh.com.tr>
2022-06-14 00:10:15 +0300

    Add project files.

---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //naming convention
+        //syntax
+        public void Ekle(Urun urun)
+        {
+
+            Console.WriteLine("Ürününüz Sepete Eklendi. : " + urun.Adi);
+
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int stokAdedi)
+        {
+            Console.WriteLine("Ürününüz Sepete Eklendi. : " + urunAdi);
+        }
+    }
+}
